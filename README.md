fb230785164bf58f38fcbcda0bfa1e579c137406
kingRywen <673895045@qq.com>
2017-11-03 17:24:52 +0800

    rem

---
+++ README.md
@@ -0,0 +1,25 @@
+# rem-flex
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+灵感来源：https://github.com/amfe/article/issues/17
+
+http://huodong.m.taobao.com/act/yibo.html
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <div class="header">
+      <h2><img :src="brannerImag"></h2>
+    </div>
+    <div class="list__wrapper">
+      <ul class="list">
+        <li v-for="(item, key) in items" :key="key">
+          <div class="li-img"><img :src="item.imgSrc"></div>
+          <div class="li-content">
+            <div class="product-title">{{ item.poductName }}</div>
+            <div class="product-price">
+              <span class="tag">双11价</span><span class="price">{{ item.price }}</span>
+            </div>
+            <div class="product-tips">{{ item.activityType }}</div>
+          </div>
+          <a href="" class="go">{{ item.activeName }}</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  mounted () {
+    import('../../data.json').then(res => {
+      console.log(res)
+      let {brannerImag, items} = res.sections[0]
+      this.brannerImag = brannerImag
+      this.items = items
+    })
+  },
+  data () {
+    return {
+      brannerImag: '',
+      items: ''
+    }
+  }
+}
+</script>
+<<style lang="scss" scoped>
+@import '../style/mixin.scss';
+</style>
